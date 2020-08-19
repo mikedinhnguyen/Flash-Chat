da67accaf08d62f640f5df58d519c98772414173
Mike <mnguyen287@ucmerced.edu>
2020-08-19 14:55:25 -0700

    initial commit

---
+++ Flash Chat iOS13/Views/Constant.swift
@@ -0,0 +1,9 @@
+//
+//  Constant.swift
+//  Clima
+//
+//  Created by Mike Nguyen on 8/18/20.
+//  Copyright © 2020 App Brewery. All rights reserved.
+//
+
+import Foundation
---
+++ Flash Chat iOS13/Views/Constants.swift
@@ -0,0 +1,9 @@
+//
+//  Constants.swift
+//  Clima
+//
+//  Created by Mike Nguyen on 8/18/20.
+//  Copyright © 2020 App Brewery. All rights reserved.
+//
+
+import Foundation
